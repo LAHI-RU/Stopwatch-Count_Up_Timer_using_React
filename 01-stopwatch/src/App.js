b9f1820f385c0b6b513ffc84a9d109b399112245
Lahiru Dhananjaya <lahiiru.dananjaya@gmail.com>
2024-04-30 11:44:56 +0530

    finish

--- 01-stopwatch/src/App.js
+++ 01-stopwatch/src/App.js
@@ -1,25 +1,59 @@
-import logo from './logo.svg';
 import './App.css';
+import {useEffect, useState } from "react";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+  const [time, setTime] = useState(0);
+  const [running, setRunning] = useState(false);
+
+
+useEffect( () => {
+  let interval;
+  if (running) {
+    interval = setInterval( () => {
+      setTime((prevTime) => prevTime + 10);
+    }, 10);
+  } else if (!running) {
+      clearInterval(interval);
+  }
+  return () => clearInterval(interval);
+}, [running]);
+
+return (
+  <div className='max-2-md flex-col items-center justify-center py-8'>
+    <h1 className='text-2xl font-semibold'> 01-Stopwatch </h1>
+    <div className='text-xl font-semibold py-4'>
+        <span> {("0" + Math.floor((time/6000) % 60)).slice(-2)}: </span>
+        <span> {("0" + Math.floor((time/1000) % 60)).slice(-2)}: </span>
+        <span> {("0" + ((time/10) % 100)).slice(-2)} </span>
     </div>
-  );
+
+    <div className='w-1/3 max-w-sm flex flex-row justify-evenly'>
+      {running ? (
+        <button 
+        className='border rounded-lg py-1 px-3.5'
+        onClick={ () => setRunning(false) }
+        > 
+        Stop 
+        
+        </button>
+      ) : (
+        <button 
+        className='border rounded-lg py-1 px-3'
+        onClick={ () => setRunning(true) }
+        > 
+        Start 
+        
+        </button>
+      )
+      }
+
+      <button 
+      className='border rounded-lg py-1 px-2.5'
+      onClick={ () => setTime(0) }> Reset </button>
+
+    </div>
+    </div>
+);
 }
 
 export default App;
